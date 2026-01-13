670072b612a24b69cce4100ce9aa3e55e910b97f
TheDubolom <feliksoid2008@gmail.com>
2026-01-13 19:45:51 +0300

    Изучили всякое

---
+++ Variables.js
@@ -0,0 +1,10 @@
+let text = "hello", text2 = "privet"
+console.log(text)
+console.log(text2)
+str = "string"
+num = 12345.6789
+bool = true
+infinite_var = 1/0 // Выдаст Infinity
+BigNum = 999999999999999999999n
+Objeect = {}
+Massive = []
